6f1e64bd94f6823fd60bcf27af9b3a8fa486ab30
Kamal Alseisy <kamal.alseisy@cartologic.com>
2017-01-30 15:38:32 +0200

    test

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'km_pypi_test',
+  packages = ['km_pypi_test'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Kamal',
+  author_email = 'kamalhg@gmail.com',
+  url = 'https://github.com/kamalhg/km_pypi_test', # use the URL to the github repo
+  download_url = 'https://github.com/kamalhg/km_pypi_test/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
