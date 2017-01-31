13ecbbbf8503b20d8136520f5b19487f9f805b2e
Kamal Alseisy <kamal.alseisy@cartologic.com>
2017-01-31 10:29:16 +0200

    v0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Kamal',
   author_email = 'kamalhg@gmail.com',
   url = 'https://github.com/kamalhg/km_pypi_test', # use the URL to the github repo
-  download_url = 'https://github.com/kamalhg/km_pypi_test/tarball/0.3', # I'll explain this in a second
+  download_url = 'https://github.com/kamalhg/km_pypi_test/tarball/0.4', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
   install_requires= [
